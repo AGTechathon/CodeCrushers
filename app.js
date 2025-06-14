181f1b575640220914dbf4427eb00b4baae4e0ee
Kashinath-Nelwade <157454670+Kashinath-Nelwade@users.noreply.github.com>
2025-06-15 02:43:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'yourSuperSecretKey',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(express.json()); // This is critical for req.body to work
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.urlencoded({ extended: false }));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +34,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter);  //he tr ahe mg kay problem
+app.use('/',usersRouter);
 app.use('/users', usersRouter);
+
+
+const habitRoutes = require('./routes/habitRoutes');
+app.use('/', habitRoutes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
